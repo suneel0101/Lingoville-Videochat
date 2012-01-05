1b1c23685cdecb5c265d9f888de7408dd03a69ba
Suneel Chakravorty <suneel0101@gmail.com>
2012-01-04 20:32:32 -0500

    changed t to ejs

--- server.js
+++ server.js
@@ -5,19 +5,117 @@
 
 var express = require('express')
   , routes = require('./routes')
+  , everyauth = require('everyauth')
 
-var app = module.exports = express.createServer();
+
 
 // Configuration
+everyauth.debug = true;
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+var usersById = {};
+var nextUserId = 0;
+
+function addUser (source, sourceUser) {
+  var user;
+  if (arguments.length === 1) { // password-based
+    user = sourceUser = source;
+    user.id = ++nextUserId;
+    return usersById[nextUserId] = user;
+  } else { // non-password-based
+    user = usersById[++nextUserId] = {id: nextUserId};
+    user[source] = sourceUser;
+  }
+  return user;
+}
+
+var usersByLogin = {
+  'brian@example.com': addUser({ login: 'brian@example.com', password: 'password'})
+};
+
+everyauth
+  .password
+    .loginWith('email')
+    .getLoginPath('/')
+    .postLoginPath('/lingotim')
+    .loginView('index.ejs')
+//    .loginLocals({
+//      title: 'Login'
+//    })
+//    .loginLocals(function (req, res) {
+//      return {
+//        title: 'Login'
+//      }
+//    })
+    .loginLocals( function (req, res, done) {
+      setTimeout( function () {
+        done(null, {
+          title: 'Async login'
+        });
+      }, 200);
+    })
+    .authenticate( function (login, password) {
+      var errors = [];
+      if (!login) errors.push('Missing login');
+      if (!password) errors.push('Missing password');
+      if (errors.length) return errors;
+      var user = usersByLogin[login];
+      if (!user) return ['Login failed'];
+      if (user.password !== password) return ['Login failed'];
+      return user;
+    })
+
+    .getRegisterPath('/register')
+    .postRegisterPath('/register')
+    .registerView('register.jade')
+//    .registerLocals({
+//      title: 'Register'
+//    })
+//    .registerLocals(function (req, res) {
+//      return {
+//        title: 'Sync Register'
+//      }
+//    })
+    .registerLocals( function (req, res, done) {
+      setTimeout( function () {
+        done(null, {
+          title: 'Async Register'
+        });
+      }, 200);
+    })
+    .validateRegistration( function (newUserAttrs, errors) {
+      var login = newUserAttrs.login;
+      if (usersByLogin[login]) errors.push('Login already taken');
+      return errors;
+    })
+    .registerUser( function (newUserAttrs) {
+      var login = newUserAttrs[this.loginKey()];
+      return usersByLogin[login] = addUser(newUserAttrs);
+    })
+
+    .loginSuccessRedirect('/lingotime')
+    .registerSuccessRedirect('/');
+
+everyauth.everymodule
+	 .findUserById( function (id, callback) {
+	  callback(null, usersById[id]);
+	 });
+
+
+var app = express.createServer(
+	    express.bodyParser()
+	  , express.static(__dirname + "/public")
+	  , express.favicon()
+	  , express.cookieParser()
+	  , express.session({ secret: 'secret',key: 'express.sid' })
+	  , everyauth.middleware()
+	);
+
+app.configure( function () {
+	  app.set('view engine', 'ejs');
+	app.set('views', __dirname + '/views');
+	app.set('view options', {layout:false});
+	});
+
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
@@ -26,7 +124,7 @@ app.configure('development', function(){
 app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
-
+everyauth.helpExpress(app);
 // Routes
 
 app.get('/', routes.index);
