2861504922cf332c87bf825a3fb9dd19086630c3
Suneel Chakravorty <suneel0101@gmail.com>
2012-01-05 15:48:30 -0500

    gradual2

--- server.js
+++ server.js
@@ -148,7 +148,7 @@ everyauth.helpExpress(app);
 app.listen(13413);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
-io = require('socket.io').listen(app);
+io = require('socket.io');
 
 
 
